7d9379fb13d56432dd27306a611227d6c1c5590f
APS229 <apsofficial229@gmail.com>
2025-06-16 14:05:29 +0530

    Implement ghost AI

--- static/index.js
+++ static/index.js
@@ -4,27 +4,27 @@ window.onload = () => {
     const MAP_SIZE = 23;
     const MAP = [
         [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-        [0, 1, 1, 1, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 1, 1, 1, 0],
-        [0, 1, 0, 1, 0, 1, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 1, 0, 1, 0, 1, 0],
-        [0, 1, 1, 1, 1, 1, 1, 1, 0, 1, 1, 1, 1, 1, 0, 1, 1, 1, 1, 1, 1, 1, 0],
-        [0, 0, 0, 1, 0, 1, 0, 1, 0, 1, 0, 0, 0, 1, 0, 1, 0, 1, 0, 1, 0, 0, 0],
-        [0, 1, 1, 1, 0, 1, 0, 1, 0, 1, 0, -1, 0, 1, 0, 1, 0, 1, 0, 1, 1, 1, 0],
-        [0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 0, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0],
-        [0, 1, 1, 1, 0, 1, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 1, 0, 1, 1, 1, 0],
-        [0, 0, 0, 0, 0, 1, 0, 1, 0, 0, 0, 0, 0, 0, 0, 1, 0, 1, 0, 0, 0, 0, 0],
-        [0, 1, 1, 1, 1, 1, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 1, 1, 1, 1, 1, 0],
-        [0, 1, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 1, 0],
-        [1, 1, 1, 1, 1, 1, 1, 1, 0, -1, -1, -1, -1, -1, 0, 1, 1, 1, 1, 1, 1, 1, 1],
-        [0, 1, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 1, 0],
-        [0, 1, 1, 1, 1, 1, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 1, 1, 1, 1, 1, 0],
-        [0, 0, 0, 0, 0, 1, 0, 1, 0, 0, 0, 0, 0, 0, 0, 1, 0, 1, 0, 0, 0, 0, 0],
-        [0, 1, 1, 1, 0, 1, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 1, 0, 1, 1, 1, 0],
-        [0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 0, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0],
-        [0, 1, 1, 1, 0, 1, 0, 1, 0, 1, 0, -1, 0, 1, 0, 1, 0, 1, 0, 1, 1, 1, 0],
-        [0, 0, 0, 1, 0, 1, 0, 1, 0, 1, 0, 0, 0, 1, 0, 1, 0, 1, 0, 1, 0, 0, 0],
-        [0, 1, 1, 1, 1, 1, 1, 1, 0, 1, 1, 1, 1, 1, 0, 1, 1, 1, 1, 1, 1, 1, 0],
-        [0, 1, 0, 1, 0, 1, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 1, 0, 1, 0, 1, 0],
-        [0, 1, 1, 1, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 1, 1, 1, 0],
+        [0, 2, 2, 2, 0, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 0, 2, 2, 2, 0],
+        [0, 2, 0, 2, 0, 2, 0, 0, 0, 0, 0, 2, 0, 0, 0, 0, 0, 2, 0, 2, 0, 2, 0],
+        [0, 2, 2, 2, 2, 2, 2, 2, 0, 2, 2, 2, 2, 2, 0, 2, 2, 2, 2, 2, 2, 2, 0],
+        [0, 0, 0, 2, 0, 2, 0, 2, 0, 2, 0, 0, 0, 2, 0, 2, 0, 2, 0, 2, 0, 0, 0],
+        [0, 2, 2, 2, 0, 2, 0, 2, 0, 2, 0, 1, 0, 2, 0, 2, 0, 2, 0, 2, 2, 2, 0],
+        [0, 2, 0, 2, 0, 2, 0, 2, 0, 2, 0, 0, 0, 2, 0, 2, 0, 2, 0, 2, 0, 2, 0],
+        [0, 2, 2, 2, 0, 2, 0, 2, 2, 2, 2, 2, 2, 2, 2, 2, 0, 2, 0, 2, 2, 2, 0],
+        [0, 0, 0, 0, 0, 2, 0, 2, 0, 0, 0, 0, 0, 0, 0, 2, 0, 2, 0, 0, 0, 0, 0],
+        [0, 2, 2, 2, 2, 2, 0, 2, 2, 2, 2, 2, 2, 2, 2, 2, 0, 2, 2, 2, 2, 2, 0],
+        [0, 2, 0, 0, 0, 0, 0, 2, 0, 0, 0, 3, 0, 0, 0, 2, 0, 0, 0, 0, 0, 2, 0],
+        [2, 2, 2, 2, 2, 2, 2, 2, 0, 1, 1, 1, 1, 1, 0, 2, 2, 2, 2, 2, 2, 2, 2],
+        [0, 2, 0, 0, 0, 0, 0, 2, 0, 0, 0, 0, 0, 0, 0, 2, 0, 0, 0, 0, 0, 2, 0],
+        [0, 2, 2, 2, 2, 2, 0, 2, 2, 2, 2, 2, 2, 2, 2, 2, 0, 2, 2, 2, 2, 2, 0],
+        [0, 0, 0, 0, 0, 2, 0, 2, 0, 0, 0, 0, 0, 0, 0, 2, 0, 2, 0, 0, 0, 0, 0],
+        [0, 2, 2, 2, 0, 2, 0, 2, 2, 2, 2, 2, 2, 2, 2, 2, 0, 2, 0, 2, 2, 2, 0],
+        [0, 2, 0, 2, 0, 2, 0, 2, 0, 2, 0, 0, 0, 2, 0, 2, 0, 2, 0, 2, 0, 2, 0],
+        [0, 2, 2, 2, 0, 2, 0, 2, 0, 2, 0, 1, 0, 2, 0, 2, 0, 2, 0, 2, 2, 2, 0],
+        [0, 0, 0, 2, 0, 2, 0, 2, 0, 2, 0, 0, 0, 2, 0, 2, 0, 2, 0, 2, 0, 0, 0],
+        [0, 2, 2, 2, 2, 2, 2, 2, 0, 2, 2, 2, 2, 2, 0, 2, 2, 2, 2, 2, 2, 2, 0],
+        [0, 2, 0, 2, 0, 2, 0, 0, 0, 0, 0, 2, 0, 0, 0, 0, 0, 2, 0, 2, 0, 2, 0],
+        [0, 2, 2, 2, 0, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 0, 2, 2, 2, 0],
         [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
     ];
 
@@ -38,16 +38,16 @@ window.onload = () => {
 
         checkMovement(direction) {
             if (direction === 'ArrowUp') {
-                if (!MAP[this.y - 1][this.x]) return;
+                if (!MAP[this.y - 1][this.x] || MAP[this.y - 1][this.x] === 3) return;
             }
             else if (direction === 'ArrowDown') {
-                if (!MAP[this.y + 1][this.x]) return;
+                if (!MAP[this.y + 1][this.x] || MAP[this.y + 1][this.x] === 3) return;
             }
             else if (direction === 'ArrowLeft') {
-                if (this.x !== 0 && !MAP[this.y][this.x - 1]) return;
+                if ((this.x !== 0 && !MAP[this.y][this.x - 1]) || MAP[this.y][this.x - 1] === 3) return;
             }
             else if (direction === 'ArrowRight') {
-                if (this.x !== MAP_SIZE - 1 && !MAP[this.y][this.x + 1]) return;
+                if ((this.x !== MAP_SIZE - 1 && !MAP[this.y][this.x + 1]) || MAP[this.y][this.x + 1] === 3) return;
             }
             else return;
             return true;
@@ -83,6 +83,7 @@ window.onload = () => {
         constructor(positionX, positionY) {
             super(positionX, positionY);
             this.currentDirection = 'ArrowRight';
+            this.steps = [];
         }
 
         move() {
@@ -103,6 +104,37 @@ window.onload = () => {
             this.color = color;
             this.num = num;
         }
+
+        getRandomSpace() {
+            let x = 0, y = 0;
+            do {
+                x = Math.floor(Math.random() * 5) + this.x - 2;
+                y = Math.floor(Math.random() * 5) + this.y - 2;
+            } while ((x === this.x && y === this.y) || x >= MAP_SIZE || x <= 0 || y >= MAP_SIZE || y <= 0 || MAP[y][x] === 0);
+            return { x, y };
+        }
+
+        pathFind(endX, endY) {
+            // Search for the player in a 5x5 radius
+            if (this.x - endX > 3 || this.x - endX < -3 || this.y - endY > 3 || this.y - endY < -3) {
+                const cords = this.getRandomSpace();
+                endX = cords.x;
+                endY = cords.y;
+            }
+            const graph = new Graph(MAP);
+            const start = graph.grid[this.y][this.x];
+            const end = graph.grid[endY][endX];
+            this.steps = astar.search(graph, start, end);
+        }
+
+        move(playerX, playerY) {
+            this.pathFind(playerX, playerY);
+            if (this.steps.length) {
+                // MAP uses Y value before X value
+                this.x = this.steps[0].y;
+                this.y = this.steps[0].x;
+            }
+        }
     }
 
     createMap();
@@ -114,7 +146,7 @@ window.onload = () => {
 
     const ghosts = [];
     for (let i = 0; i < 4; i++) {
-        const ghost = new Ghost(i > 1 ? 10 + i: 9 + i, 11, GHOST_COLORS[i], i);
+        const ghost = new Ghost(i > 1 ? 10 + i : 9 + i, 11, GHOST_COLORS[i], i);
         ghosts.push(ghost);
         const ghostElement = document.createElement('div');
         ghostElement.id = `ghost ${i}`;
@@ -133,11 +165,12 @@ window.onload = () => {
             for (let j = 0; j < MAP_SIZE; j++) {
                 const space = document.createElement('td');
                 space.id = `${j} ${i}`;
-                if (!MAP[i][j]) space.className = 'edge';
-                else if (MAP[i][j] === 1) {
-                    const coin = document.createElement('div');
-                    coin.className = 'coin';
-                    space.append(coin);
+                if (i === 10 && j === 11) space.className = 'gate';
+                else if (!MAP[i][j]) space.className = 'edge';
+                else if (MAP[i][j] === 2) {
+                    const particle = document.createElement('div');
+                    particle.className = 'particle';
+                    space.append(particle);
                 }
                 row.append(space);
             }
@@ -149,7 +182,7 @@ window.onload = () => {
         if (started) return;
         started = true;
         updateGame();
-        setInterval(() => {
+        window.gameInterval = setInterval(() => {
             updateGame();
         }, GAME_SPEED);
 
@@ -162,12 +195,20 @@ window.onload = () => {
         if (player.move()) {
             const playerElement = document.getElementById('player');
             const positionElement = document.getElementById(`${player.x} ${player.y}`);
-            const coin = positionElement.getElementsByClassName('coin')[0];
-            if (coin) {
-                coin.remove();
-                MAP[player.y][player.x] = -1;
+            const particle = positionElement.getElementsByClassName('particle')[0];
+            if (particle) {
+                particle.remove();
+                MAP[player.y][player.x] = 1;
             }
             positionElement.append(playerElement);
+        }
+        for (let i = 0; i < 4; i++) {
+            const ghost = ghosts[i];
+            ghost.move(player.x, player.y);
+            const ghostElement = document.getElementById(`ghost ${i}`);
+            const positionElement = document.getElementById(`${ghost.x} ${ghost.y}`);
+            positionElement.append(ghostElement);
+            if (ghost.x === player.x && ghost.y === player.y) return clearInterval(window.gameInterval);
         }
     }
 };
